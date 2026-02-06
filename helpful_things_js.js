c5f9ddc7d4bbe346c9203aa371cf3f0335eddb1a
kayceedee <127042580+kayceedee@users.noreply.github.com>
2026-02-06 17:30:09 -0500

    add random

--- helpful_things_js.js
+++ helpful_things_js.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // BASIC HOMEPAGE FUNCTIONS
+
   let runenv = "unknown"
 
   if (["http:", "https:"].includes(window.location.protocol)) {
@@ -9,6 +11,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
   console.log(runenv)
 
+  async function sleep(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+  }
 
   function getlink(link) {
     if (runenv == "web") {
@@ -34,10 +39,21 @@ document.addEventListener("DOMContentLoaded", () => {
     window.location.href = getlink("#flashcards")
   })
 
-  for (let i=0; i<totop.length; i++)
+  for (let i=0; i<totop.length; i++) {
     totop[i].addEventListener("click", () => {
       window.location.href = getlink("#")
     })
+  }
+
+  // RANDOM NUMBER GENERATOR FUNTIONALITY
+
+  let randomnumdisplay = document.getElementById("randomnumdisplay")
+  let randomnummin = document.getElementById("min")
+  let randomnummax = document.getElementById("max")
+  let genrandomnum = document.getElementById("genrandomnum")
+
+  genrandomnum.addEventListener("click", () => {
+    randomnumdisplay.innerHTML = (Math.floor(Math.random() * (randomnummax.value - randomnummin.value) + randomnummin.value))
+  })
+
 })
-
-
