c3897d0750e4f3e1f481b74c8d9552ec1d333d00
kayceedee <127042580+kayceedee@users.noreply.github.com>
2026-02-05 19:17:23 -0500

    Update helpful_things_js.js

--- helpful_things_js.js
+++ helpful_things_js.js
@@ -1,1 +1,43 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let runenv = "unknown"
+
+  if (["http:", "https:"].includes(window.location.protocol)) {
+    runenv = "web"
+  } else if (window.location.protocol === "file:") {
+    runenv = "local"
+  }
+
+  console.log(runenv)
+
+
+  function getlink(link) {
+    if (runenv == "web") {
+      return "https://kayceedee.github.io/" + link
+    } else if (runenv == "local") {
+      return "file:///C:/Users/super/Documents/JamesCoding/helpful_things.html/" + link
+    }
+  }
+
+
+  let torandomnum = document.getElementById("torandomnum")
+  let tofractions = document.getElementById("tofractions")
+  let toflashcards = document.getElementById("toflashcards")
+  let totop = document.getElementsByClassName("totop")
+
+  torandomnum.addEventListener("click", () => {
+    window.location.href = getlink("#randomnum")
+  })
+  tofractions.addEventListener("click", () => {
+    window.location.href = getlink("#fractions")
+  })
+  toflashcards.addEventListener("click", () => {
+    window.location.href = getlink("#flashcards")
+  })
+
+  for (let i=0; i<totop.length; i++)
+    totop[i].addEventListener("click", () => {
+      window.location.href = getlink("#")
+    })
+})
+
 
